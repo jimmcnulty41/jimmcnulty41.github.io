06bbd2c3f366ea8dee0a3740585e426c7dc0ae14
James McNulty <jimmcnulty41@gmail.com>
2018-07-08 16:15:01 -0700

    Style improvements, more goofiness

--- script.js
+++ script.js
@@ -1,10 +1,58 @@
 window.onload = () => {
-    var anim = document.getElementById('anim');
-    anim.onmouseenter = () => {
-        anim.src = "assets/animator2.png";
+    setAnim();
+    linkImagesToText();
+};
+
+const ANIM_1_PATH = "assets/animation1.png";
+const ANIM_2_PATH = "assets/animation2.png";
+
+const setAnim = () => {
+    const anim = document.getElementById("anim");
+    const observer = new MutationObserver(mutationsList => {
+        if (mutationsList[0].attributeName !== "class") return;
+        anim.src =
+            anim.currentSrc.indexOf(ANIM_1_PATH) === -1
+                ? ANIM_1_PATH
+                : ANIM_2_PATH;
+    });
+    observer.observe(anim, { attributes: true });
+};
+
+const HOVER_CLASS = "linkedHover";
+
+const linkImagesToText = () => {
+    const images = document.getElementsByTagName("img");
+    const linkedTexts = document.getElementsByClassName("linked");
+    for (let i = 0; i < images.length; ++i) {
+        const setHover = () => {
+            images[i].classList.add(HOVER_CLASS);
+            linkedTexts[i].classList.add(HOVER_CLASS);
+        };
+        const unsetHover = () => {
+            images[i].classList.remove(HOVER_CLASS);
+            linkedTexts[i].classList.remove(HOVER_CLASS);
+        };
+        addHoverBehaviorToElement(
+            [images[i], linkedTexts[i]],
+            setHover,
+            unsetHover
+        );
     }
-    anim.onmouseleave = () => {
-        anim.src = "assets/animator1.png";
+};
+
+const isDesktop = () =>
+    window.matchMedia("only screen and (min-width: 760px)").matches;
+
+const addHoverBehaviorToElement = (elements, onBehavior, offBehavior) => {
+    if (isDesktop()) {
+        elements.forEach(element => {
+            element.onmouseenter = onBehavior;
+            element.onmouseleave = offBehavior;
+        });
+    } else {
+        elements.forEach(element => {
+            element.ontouchstart = onBehavior;
+            element.ontouchend = offBehavior;
+        });
     }
-    console.log('suh');
-}
+};
