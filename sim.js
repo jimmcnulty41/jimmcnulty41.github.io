6d50902168b8dfad25a5b54e51f07760391d310e
James McNulty <jimmcnulty41@gmail.com>
2023-01-17 16:51:58 -0500

    Add Grid
    not my favorite grid implementation

--- sim.js
+++ sim.js
@@ -18,6 +18,78 @@ function start() {
   update();
 }
 
+function getLine({ axis, length, alt = { x: 0, y: 0, z: 0 } }) {
+  switch (axis) {
+    case "x":
+      return new THREE.Line(
+        new THREE.BufferGeometry().setFromPoints([
+          new THREE.Vector3(0, alt.y, alt.z),
+          new THREE.Vector3(length, alt.y, alt.z),
+        ]),
+
+        new THREE.LineBasicMaterial({ color: 0xff0000 })
+      );
+    case "y":
+      return new THREE.Line(
+        new THREE.BufferGeometry().setFromPoints([
+          new THREE.Vector3(alt.x, 0, alt.z),
+          new THREE.Vector3(alt.x, length, alt.z),
+        ]),
+
+        new THREE.LineBasicMaterial({ color: 0x00ff00 })
+      );
+    case "z":
+      return new THREE.Line(
+        new THREE.BufferGeometry().setFromPoints([
+          new THREE.Vector3(alt.x, alt.y, 0),
+          new THREE.Vector3(alt.x, alt.y, length),
+        ]),
+        new THREE.LineBasicMaterial({ color: 0x0000ff })
+      );
+    default:
+      throw new Error("unexpected axis");
+  }
+}
+
+function remap(min, max, newMin, newMax) {
+  return (input) => newMin + ((input - min) / (max - min)) * (newMax - newMin);
+}
+
+function getGrid() {
+  const n = 20;
+  const xParent = new THREE.Object3D();
+  [...Array(n)]
+    .map((e, i) =>
+      getLine({
+        axis: "x",
+        length: remap(0, n, 100, 10)(i),
+        alt: { x: 0, y: i * 10, z: 0 },
+      })
+    )
+    .forEach((x) => xParent.add(x));
+  const yParent = new THREE.Object3D();
+  [...Array(n)]
+    .map((e, i) =>
+      getLine({
+        axis: "y",
+        length: remap(0, n, 100, 10)(i),
+        alt: { x: i * 10, y: 0, z: 00 },
+      })
+    )
+    .forEach((x) => xParent.add(x));
+  const zParent = new THREE.Object3D();
+  [...Array(n)]
+    .map((e, i) =>
+      getLine({
+        axis: "z",
+        length: remap(0, n, 100, 10)(i),
+        alt: { x: i * 10, y: 0, z: 0 },
+      })
+    )
+    .forEach((x) => xParent.add(x));
+  return xParent;
+}
+
 function createObject({ type }) {
   switch (type) {
     case "sphere":
@@ -26,6 +98,8 @@ function createObject({ type }) {
         new THREE.MeshBasicMaterial({ color: 0xffffff })
       );
       return mesh;
+    case "grid":
+      return getGrid();
   }
 }
 
@@ -33,8 +107,18 @@ function updateScene() {}
 
 let model = {
   time: 0,
-  entities: [],
-  idCounter: 0,
+  entities: [
+    {
+      id: 0,
+      components: {
+        render: {
+          type: "grid",
+        },
+        position: { x: 0, y: 0, z: 0 },
+      },
+    },
+  ],
+  idCounter: 1,
   sceneMapping: {},
 };
 
