4c2722fe6ce611f3134b0aa23848e16234869901
James McNulty <jimmcnulty41@gmail.com>
2023-01-19 22:09:38 -0500

    Let there be color!

--- sims/build/systems/updateTHREESceneSystem.js
+++ sims/build/systems/updateTHREESceneSystem.js
@@ -13,25 +13,46 @@ const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerH
 camera.position.set(100, 100, 100);
 camera.lookAt(0, 0, 0);
 const orbitControls = new OrbitControls(camera, canvas);
-const geometry = new THREE.IcosahedronGeometry(0.5, 3);
+const geometry = new THREE.IcosahedronGeometry(1, 3);
 const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
 const instancedMesh = new THREE.InstancedMesh(geometry, material, 100);
 instancedMesh.instanceMatrix.setUsage(THREE.DynamicDrawUsage); // will be updated every frame
 scene.add(instancedMesh);
-let instanceId = 0;
+let instanceIdCounter = 0;
 const matrix_reg = new THREE.Matrix4();
+const color_reg = new THREE.Color(1, 1, 1);
+function remap(min, max, newMin, newMax, clamp) {
+    return (input) => {
+        const val = newMin + ((input - min) / (max - min)) * (newMax - newMin);
+        if (clamp) {
+            return Math.max(newMin, Math.min(newMax, val));
+        }
+        else {
+            return val;
+        }
+    };
+}
 function updateSphere(sphereEntity) {
     const id = entityIdToInstanceId[sphereEntity.id];
     matrix_reg.identity();
+    color_reg.setRGB(1, 1, 1);
     if (id === undefined) {
         matrix_reg.setPosition(0, 0, 0);
-        entityIdToInstanceId[sphereEntity.id] = instanceId;
-        instancedMesh.setMatrixAt(instanceId++, matrix_reg);
+        entityIdToInstanceId[sphereEntity.id] = instanceIdCounter;
+        instancedMesh.setMatrixAt(instanceIdCounter++, matrix_reg);
+        instancedMesh.setColorAt(instanceIdCounter, new THREE.Color(0xffffff));
     }
     else {
         const { x, y, z } = sphereEntity.components.position;
         matrix_reg.setPosition(x, y, z);
         instancedMesh.setMatrixAt(id, matrix_reg);
+        const dist = Math.sqrt(x * x + z * z);
+        //color_reg.r = remap(0, 10, 0, 1, true)(dist);
+        const w = sphereEntity.components.wander;
+        if (w) {
+            color_reg.g = remap(0, 1, 0, 1, true)(w.speed);
+        }
+        instancedMesh.setColorAt(id, color_reg);
     }
 }
 function updateGrid(gridEntity) {
@@ -52,6 +73,9 @@ export function updateTHREEScene(model) {
         .filter((e) => e.components.render.type === "sphere")
         .forEach(updateSphere);
     instancedMesh.instanceMatrix.needsUpdate = true;
+    if (instancedMesh.instanceColor) {
+        instancedMesh.instanceColor.needsUpdate = true;
+    }
     renderables
         .filter((e) => e.components.render.type === "grid")
         .forEach(updateGrid);
