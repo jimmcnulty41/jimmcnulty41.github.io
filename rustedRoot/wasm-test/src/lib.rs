7ac593f2ea41643bafa6737ebcb069a6bebb4591
James McNulty <jimmcnulty41@gmail.com>
2023-11-01 03:43:42 -0400

    Only run link function body once

--- rustedRoot/wasm-test/src/lib.rs
+++ rustedRoot/wasm-test/src/lib.rs
@@ -4,8 +4,12 @@ use bevy::{
     asset::LoadState,
     gltf::{Gltf, GltfNode},
     prelude::*,
+    scene::{self, InstanceId},
 };
-use bevy_mod_picking::*;
+use bevy_mod_picking::{
+    prelude::{Click, Listener, On, Pointer},
+    *,
+};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -17,7 +21,11 @@ pub enum Cell {
 }
 
 #[derive(Resource)]
-struct PinkSynthHandle(Handle<Gltf>, bool);
+struct PinkSynthHandle {
+    pub gltf_handle: Handle<Gltf>,
+    pub scene_spawner_id: Option<InstanceId>,
+    pub is_loaded: bool,
+}
 
 fn setup(
     mut commands: Commands,
@@ -25,10 +33,11 @@ fn setup(
     asset_server: Res<AssetServer>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    commands.insert_resource(PinkSynthHandle(
-        asset_server.load("../../assets/models/pink_synth.glb"),
-        false,
-    ));
+    commands.insert_resource(PinkSynthHandle {
+        gltf_handle: asset_server.load("../../assets/models/pink_synth.glb"),
+        scene_spawner_id: None,
+        is_loaded: false,
+    });
 
     commands.spawn(PbrBundle {
         mesh: meshes.add(shape::Plane::from_size(5.0).into()),
@@ -61,25 +70,59 @@ fn sys_spawn_on_load(
     mut synth: ResMut<PinkSynthHandle>,
     mut scene_spawner: ResMut<SceneSpawner>,
 ) {
-    if synth.1 {
+    if synth.is_loaded {
         return;
     }
-    if asset_server.get_load_state(&synth.0) == LoadState::Loaded {
-        let gltf = gltf_assets.get(&synth.0).unwrap();
+    if asset_server.get_load_state(&synth.gltf_handle) == LoadState::Loaded {
+        let gltf = gltf_assets.get(&synth.gltf_handle).unwrap();
         let gltf_scene_handle = gltf.scenes.get(0).unwrap();
-        scene_spawner.spawn(gltf_scene_handle.clone_weak());
-        synth.1 = true;
+        synth.scene_spawner_id = Some(scene_spawner.spawn(gltf_scene_handle.clone_weak()));
+        synth.is_loaded = true;
         info!("spawning scene...")
     }
 }
 
+#[derive(Resource)]
+struct KeysLinked(bool);
+
 fn sys_link_synth_keys(
     mut commands: Commands,
-    scene_spawner: Res<SceneSpawner>,
-    gltf_nodes: Res<Assets<GltfNode>>,
-    query: Query<&Handle<GltfNode>>,
+    mut synth: ResMut<PinkSynthHandle>,
+    gltf_assets: Res<Assets<Gltf>>,
+    mut scenes: ResMut<Assets<Scene>>,
+    mut scene_spawner: ResMut<SceneSpawner>,
+    mut keys_linked: ResMut<KeysLinked>,
+    animations_q: Query<Entity, With<Handle<Mesh>>>,
 ) {
-    info!("sys_link_synth_keys");
+    if keys_linked.0 {
+        return;
+    }
+    info!("sys_link_synth_keys: BEGIN");
+    if let Some(gltf) = gltf_assets.get(&synth.gltf_handle) {
+        if let Some(scene_handle) = &gltf.default_scene {
+            if let Some(scene) = scenes.get_mut(scene_handle) {
+                // get the right entity
+                for e in animations_q.iter() {
+                    commands.entity(e).insert((
+                        PickableBundle::default(),
+                        On::<Pointer<Click>>::run(|ev: Listener<Pointer<Click>>| {
+                            info!("Clicked {:?}", ev.target);
+                        }),
+                    ));
+                }
+
+                keys_linked.0 = true;
+                info!("sys_link_synth_keys: scene is loaded and manipulated");
+            } else {
+                info!("sys_link_synth_keys: MISSING scene");
+            }
+        } else {
+            info!("sys_link_synth_keys: MISSING scene handle");
+        }
+    } else {
+        info!("sys_link_synth_keys: MISSING GLTF");
+    }
+    info!("sys_link_synth_keys: END");
 }
 
 #[wasm_bindgen(start)]
@@ -93,6 +136,7 @@ pub fn bevy_main() {
             }),
             ..default()
         }))
+        .insert_resource(KeysLinked(false))
         .add_plugins(DefaultPickingPlugins)
         .add_systems(Startup, setup)
         .add_systems(PreUpdate, (sys_spawn_on_load, sys_link_synth_keys))
